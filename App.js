18e701b1758c2158ce9b55330e731e15ed7ec1a6
Celso Lopes <celso.lopes.jr@gmail.com>
2023-07-19 22:12:40 -0300

    Iniciando o Projeto para acessar a câmera do celular

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import { FontAwesome } from "@expo/vector-icons"
+import { useState, useEffect, useRef } from 'react';
 
 export default function App() {
+
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  const [hasPermission, setHasPermission] = useState(null)
+
+  const camRef = useRef(null)
+  const [capturedPhoto, setCapturedPhoto] = useState(null)
+
+  useEffect (() => {
+    (async () => {
+      const {status} = await Camera.requestCameraPermissionsAsync()
+      setHasPermission(status === "granted");
+    }) ();
+  }, [])
+
+  if(hasPermission === null){
+    return
+    <View/>
+  }
+  if(hasPermission === false){
+    return
+    <Text>Acesso Negado</Text>
+  }
+
+  async function takePicture(){
+    if (camRef){
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri)
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera
+        style={styles.Camera}
+        type={type}
+      >
+        <View style={styles.contentButtons}>
+          <TouchableOpacity
+          style={styles.buttonFlip}
+          onPress={() =>{ 
+            setType(
+              type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+            )
+          }}
+          >
+            <FontAwesome name="exchange" size={23} color={"red"}>              
+            </FontAwesome>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={styles.buttonCamera} 
+            onPress={takePicture}
+          >
+            <FontAwesome name="camera" size={23} color={"#ffffff"}>             
+            </FontAwesome>
+
+          </TouchableOpacity>
+        </View>
+
+      </Camera>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  Camera: {
+    width: "100%",
+    height: "100%",
+  },
+  contentButtons:{
+    flex: 1,
+    backgroundColor: "transparente",
+    flexDirection: "row",
+  },
+  buttonFlip: {
+    position: "absolute",
+    bottom: 50,
+    left: 30,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 20,
+  },
+  buttonCamera:{
+    position: "absolute",
+    bottom: 50,
+    right: 30,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 20,
+  }
+
 });
